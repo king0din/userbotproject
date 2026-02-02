812d34db72ba6cdbeff32b43a98c792ea2c0b12c
king odin <76708652+king0din@users.noreply.github.com>
2026-02-02 04:51:04 +0300

    ❤️

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ load_dotenv()
 
 import config
 from database import database as db
-from userbot_compat import userbot_manager, plugin_manager
+
+# userbot modülünü DOĞRUDAN import et (uyumluluk katmanından önce)
+from userbot.manager import userbot_manager
+from userbot.plugins import plugin_manager
+
 from handlers import register_user_handlers, register_admin_handlers
 from utils import send_log, get_readable_time
 
