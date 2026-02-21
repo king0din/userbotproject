4afb98420f2d7a2856d548e2535abe72a32fe198
king odin <76708652+king0din@users.noreply.github.com>
2026-02-21 19:25:28 +0300

    ❤️

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,5 +4,6 @@
 
 from .user import register_user_handlers
 from .admin import register_admin_handlers
+from .chess import register_chess_handlers, CHESS_GAMES, create_buttons, build_text
 
-__all__ = ['register_user_handlers', 'register_admin_handlers']
+__all__ = ['register_user_handlers', 'register_admin_handlers', 'register_chess_handlers', 'CHESS_GAMES', 'create_buttons', 'build_text']
