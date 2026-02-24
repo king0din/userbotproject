7dc48fbcff1027335a52a9872a728e461aff12bd
king odin <76708652+king0din@users.noreply.github.com>
2026-02-24 18:16:10 +0300

    ❤️

--- handlers/user.py
+++ handlers/user.py
@@ -92,6 +92,7 @@ def register_user_handlers(bot):
             btn.callback("❓ Yardım", "help_main",
                         icon_custom_emoji_id=5238091390690068061),
             btn.callback("📝 Komutlar", "commands")
+                        icon_custom_emoji_id=5962952497197748583)
         ])
         
         # Plugin Kanalı
